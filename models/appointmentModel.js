fdcd3960dae2ee6cadf87e2f2d62e9a3e6994e4a
Shriya Sorte <shriyasorte2000@gmail.com>
2024-06-20 11:48:48 +0530

    changes

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -15,5 +15,4 @@ module.exports = Appointment;
 //     "patientId": "6668184965e3ecd3ff948203",
 //     "doctorId":"66680240ff10df96bfee0c72",
 //     "appointmentDateTime": "2024-06-18T12:44:01",
-//     "status":"Pending"
 // }
